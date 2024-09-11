088199292b71ced8fd49cd6cbe7baf13e2b9a8de
roshan <roshan04dec@gmail.com>
2024-09-10 21:51:15 -0500

    added metadata forwarder interceptor for daprClient

--- client/invoke.go
+++ client/invoke.go
@@ -18,7 +18,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"google.golang.org/grpc/metadata"
 	"strings"
 
 	"google.golang.org/protobuf/types/known/anypb"
@@ -77,7 +76,6 @@ func hasRequiredInvokeArgs(appID, methodName, verb string) error {
 
 // InvokeMethod invokes service without raw data ([]byte).
 func (c *GRPCClient) InvokeMethod(ctx context.Context, appID, methodName, verb string) (out []byte, err error) {
-	ctx = metadata.AppendToOutgoingContext(ctx, "dapr-app-id", appID)
 	if err := hasRequiredInvokeArgs(appID, methodName, verb); err != nil {
 		return nil, fmt.Errorf("missing required parameter: %w", err)
 	}
